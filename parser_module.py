7416ec393b0de37e6c4462274f491bf8bc04b5c9
shakedal <salmog510@gmail.com>
2020-11-26 18:34:30 +0200

    we're the RE masters

--- parser_module.py
+++ parser_module.py
@@ -19,13 +19,129 @@ SPLIT_URL_PATTERN = "://|\?|/|=|-|(?<=www)."
 REMOVE_URL_PATTERN = r"http\S+"
 HASHTAG_PATTERN = r'_|(?<=[^A-Z])(?=[A-Z])'
 TWITTER_STATUS_PATTERN = r'(twitter.com\/)(\S*)(\/status\/)(\d)*'
-TOKENIZER_PATTERN = r'''(?x)\d+\ +\d+\/\d+|\d+\/\d+|\d+\.*\d*(?:[MKB])*(?:[$%])|(?:[A-Z]\.)+| \w+(?:-\w+)*| \$?\d+(?:\.\d+)?%?'''
+TOKENIZER_PATTERN = r'''(?x)\d+\ +\d+\/\d+|\d+\/\d+|\d+\.*\d*(?:[MKB])*(?:[$%])*|(?:[A-Z]\.)+| (?:[#@])*\w+(?:\'\w+)*| \$?\d+(?:\.\d+)?%?'''
 
+contractions = {
+"ain't": "is not",
+"aren't": "are not",
+"can't": "cannot",
+"can't've": "cannot have",
+"'cause": "because",
+"could've": "could have",
+"couldn't": "could not",
+"couldn't've": "could not have",
+"didn't": "did not",
+"doesn't": "does not",
+"don't": "do not",
+"hadn't": "had not",
+"hadn't've": "had not have",
+"hasn't": "has not",
+"haven't": "have not",
+"he'd": "he had",
+"he'd've": "he would have",
+"he'll": "he will",
+"he'll've": "he will have",
+"he's": "he is",
+"how'd": "how did",
+"how'd'y": "how do you",
+"how'll": "how will",
+"how's": "how is",
+"I'd": "I would",
+"I'd've": "I would have",
+"I'll": "I will",
+"I'll've": "I will have",
+"I'm": "I am",
+"I've": "I have",
+"isn't": "is not",
+"it'd": "it had",
+"it'd've": "it would have",
+"it'll": "it will",
+"it'll've": "it will have",
+"it's": "it is",
+"let's": "let us",
+"ma'am": "madam",
+"mayn't": "may not",
+"might've": "might have",
+"mightn't": "might not",
+"mightn't've": "might not have",
+"must've": "must have",
+"mustn't": "must not",
+"mustn't've": "must not have",
+"needn't": "need not",
+"needn't've": "need not have",
+"o'clock": "of the clock",
+"oughtn't": "ought not",
+"oughtn't've": "ought not have",
+"shan't": "shall not",
+"sha'n't": "shall not",
+"shan't've": "shall not have",
+"she'd": "she had",
+"she'd've": "she would have",
+"she'll": "she will",
+"she'll've": "she will have",
+"she's": "she is",
+"should've": "should have",
+"shouldn't": "should not",
+"shouldn't've": "should not have",
+"so've": "so have",
+"so's": "so is",
+"that'd": "that would",
+"that'd've": "that would have",
+"that's": "that is",
+"there'd": "there would",
+"there'd've": "there would have",
+"there's": "there is",
+"they'd": "they would",
+"they'd've": "they would have",
+"they'll": "they will",
+"they'll've": "they will have",
+"they're": "they are",
+"they've": "they have",
+"to've": "to have",
+"wasn't": "was not",
+"we'd": "we would",
+"we'd've": "we would have",
+"we'll": "we will",
+"we'll've": "we will have",
+"we're": "we are",
+"we've": "we have",
+"weren't": "were not",
+"what'll": "what will",
+"what'll've": "what will have",
+"what're": "what are",
+"what's": "what is",
+"what've": "what have",
+"when's": "when is",
+"when've": "when have",
+"where'd": "where did",
+"where's": "where is",
+"where've": "where have",
+"who'll": "who will",
+"who'll've": "who will have",
+"who's": "who is",
+"who've": "who have",
+"why's": "why is",
+"why've": "why have",
+"will've": "will have",
+"won't": "will not",
+"won't've": "will not have",
+"would've": "would have",
+"wouldn't": "would not",
+"wouldn't've": "would not have",
+"y'all": "you all",
+"y'all'd": "you all would",
+"y'all'd've": "you all would have",
+"y'all're": "you all are",
+"y'all've": "you all have",
+"you'd": "you would",
+"you'd've": "you would have",
+"you'll": "you will",
+"you'll've": "you will have",
+"you're": "you are",
+"you've": "you have"
+}
 
 # TODO: entities detector
-# TODO: add removement of ' (chukus)
-# TODO: CHECK IF NEEDED TO REMOVE the at - at hashtags example THE DOLLAR
-# TODO: 35 3/4 term
 
 class Parse:
 
@@ -46,17 +162,25 @@ class Parse:
         :return:
         """
         term_dict = {}
+
         text_tokens = re.findall(TOKENIZER_PATTERN, text)
         indices_counter = 0
         for term in text_tokens:
             indices_counter += 1
-            if term[0] == "#":
+            if term[0] == "#":  # handle hashtags
                 hashtag_list = self.hashtag_parser(term)
                 for mini_term in hashtag_list:
                     self.dictAppender(term_dict, indices_counter, mini_term)
-            elif term[0] == "@":
+            elif term[0] == "@":  # handle tags
                 no_tag = self.tags_parser(term)
                 self.dictAppender(term_dict, indices_counter, no_tag)
+            elif term in contractions:  # remove things like he'll
+                new_terms = contractions[term].split(" ")
+                for mini_term in new_terms:
+                    self.dictAppender(term_dict, indices_counter, mini_term)
+                    indices_counter += 1
+                indices_counter -= 1
+                continue
             self.dictAppender(term_dict, indices_counter, term)
 
         return term_dict, indices_counter
